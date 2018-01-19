b5396d59bf205598c6a2f900b75ab4752f3bf9be
Damien Stanton <damien.stanton@gmail.com>
2018-01-19 17:21:18 -0500

    bump copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
-        <p className="copyright">&copy; 2017 Damien Stanton</p>
+        <p className="copyright">&copy; 2018 Damien Stanton</p>
     </footer>
 )
 
